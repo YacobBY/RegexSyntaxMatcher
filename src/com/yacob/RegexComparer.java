058bd5f9f7cacd224e0174126c59f429c58d4f07
Unknown <yacob101@gmail.com>
2019-02-05 16:45:27 +0100

    Swapper made

--- src/com/yacob/RegexComparer.java
+++ src/com/yacob/RegexComparer.java
@@ -6,62 +6,91 @@ import java.util.Scanner;
 
 public class RegexComparer {
 
-	private static Scanner scan;
+    private static Scanner scan;
 
-	public static void main(String[] args) {
-		scan = new Scanner(System.in);
-		String input;
+    public static void main(String[] args) {
+        scan = new Scanner(System.in);
+        String input;
 
-		System.out.println("Enter Regex please.");
-		input = scan.nextLine();
-		System.out.println(input);
-		List<Character> regex = new ArrayList<Character>();
+//        System.out.println("Enter Regex please.");
+//        input = scan.nextLine();
+        input = "xya(zxya)*z";
+        System.out.println(input);
 
-		for (int i = 0; i < input.length(); i++) {
-			regex.add(input.charAt(i));
-		}
+        List<Character> regex = new ArrayList<Character>();
+        for (int i = 0; i < input.length(); i++) {
+            regex.add(input.charAt(i));
+        }
+        List<String> result = new ArrayList<String>();
+        String temp = "";
+        for (int i = 0; i < regex.size(); i++) {
+            //
+            if (regex.get(i).equals('(')) {
+                temp += '(';
+                while (i + 2 <= regex.size() - 1 && regex.get(i + 1) != ')') {
+                    i++;
+                    temp += regex.get(i);
+                }
+                temp += ')';
+                result.add(temp);
+                i += 2;
+            }
+            result.add(regex.get(i).toString());
+            if (regex.get(i).equals('+')) {
 
-		List<String> result = new ArrayList<String>();
-		String temp = "";
-		for (int i = 0; i < regex.size(); i++) {
-			//
-			if (regex.get(i).equals('(')) {
-				temp += '(';
-				while (regex.get(i + 1) != ')') {
-					i++;
-					temp += regex.get(i);
-				}
-				temp += ')';
-				result.add(temp);
-				i+=2;
-				
-			}
-			result.add(regex.get(i).toString());
+                if (temp.contains("(")) {
+                    result.remove(result.size() - 1);
+                } else {
+                    result.remove(result.size() - 1);
+                    temp = result.get(result.size() - 1);
+                }
+                temp += '*';
+                result.add(temp);
+                temp = "";
+            }
+        }
 
-			if (regex.get(i).equals('+')) {
+        System.out.println("ArrayList -> result");
+        System.out.println(result);
+        int index = 0;
+        for (String s : result)
+            System.out.println((index++) + ": " + s);
 
-				if (temp.contains("(")) {
-					result.remove(result.size() - 1);
-				} else {
-					result.remove(result.size() - 1);
-					temp = result.get(result.size() - 1);
+        for (int i = 0; i < result.size() - 1; i++) {
+            //Get index with (
+            if (i > 0 && (result.get(i).charAt(0) == '(')) {
+                //last character before the )
+//                  if (result.get(i-1).charAt(result.get(i).length()-2) ==  )
 
-				}
+                if ((result.get(i).charAt(result.get(i).length() - 2)) == (result.get(i - 1).charAt(result.get(i - 1).length() - 1))) {
+                    System.out.println((result.get(i).charAt(result.get(i).length() - 2)));
+                    System.out.println((result.get(i - 1).charAt(result.get(i - 1).length() - 1)));
+                    //Switch last character before ")" and last character in slot before, and then switch the entire slots.
+                    StringBuilder charSwapper = new StringBuilder();
+                    charSwapper.append(result.get(i - 1).charAt(result.get(i - 1).length() - 1));
+                    charSwapper.append(result.get(i).substring(1, result.get(i).length() - 2));
+                    charSwapper.insert(0, '(');
+                    charSwapper.append(")");
+                    result.set(i, String.valueOf(result.get(i - 1).charAt(result.get(i - 1).length() - 1)));
+                    result.set(i-1, charSwapper.toString());
+                    System.out.println(charSwapper.toString());
+                    i=0;
+                    
+                }
+            }
+        }
+        System.out.println("-----------------------------");
+        StringBuilder finalString = new StringBuilder();
+        index=0;
+        for (String s : result){
+            finalString.append(s);
+            System.out.println((index++) + ": " + s);}
+        System.out.println(finalString);
 
-				temp += '*';
-				result.add(temp);
-				temp = "";
-			}
+    }
 
-		}
-
-		System.out.println("ArrayList -> result");
-		System.out.println(result);
-		int index = 0;
-		for (String s : result)
-			System.out.println((index++) + ": " + s);
-	}
 }
+
 /*
 Tussen haakjes plus vervangen door res te gebruiken ipv result met 2e functie
 
@@ -71,6 +100,7 @@ verwisseld
 
 Wanneer de laatste letter tussen haakjes hetzelfde is als de laatste letter voor de haakjes kunnen deze allebei naar rechts verschoven worden, example:
 
-String regexOne = "xy(zxy)*z"; 
+String regexOne = "xyz(zxy)+z";
+String regexOne = "x(yzx)*yz";
 String regexTwo = "(xyz)*xyz";
 */
